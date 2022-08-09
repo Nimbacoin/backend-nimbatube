ae621dd428a3cfea42e547754c630269b635d632
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-08-10 00:58:49 +0200

    DFSDS

--- routes/chanel/post/chanelposts.js
+++ routes/chanel/post/chanelposts.js
@@ -1,41 +1,22 @@
 import express from "express";
 import AuthToken from "../../../utils/verify-user/VerifyUser.js";
 import createNewChanelGeneral from "./create-new-chanel/createNewChanelGeneral.js";
-import routerCreateNewChanelOther from "./create-new-chanel/createNewChanelOther.js";
 const routesChanelPosts = express.Router();
 
 const allRoutes = [
   {
     name: createNewChanelGeneral,
     auth: true,
-    rout: "/create-new-chanel/general",
-  },
-  {
-    name: routerCreateNewChanelOther,
-    auth: true,
-    rout: "/create-new-chanel/other",
+    rout: "/create-new-chanel",
   },
 ];
-
-// routesChanelPosts.use(
-//   `/post/chanel/create-new-chanel/general:token`,
-//   AuthToken,
-//   createNewChanelGeneral
-// );
 
 allRoutes.map(({ name, auth, rout }) => {
   if (auth) {
     routesChanelPosts.use(`/post/chanel${rout}:token`, AuthToken, name);
   } else {
-    console.log("here 222344333333333");
     routesChanelPosts.use(`/post/chanel${rout}:token`, name);
   }
 });
-
-// routesChanelPosts.use(
-//   `/post/chanel/create-new-chanel/general:token`,
-//   AuthToken,
-//   createNewChanelGeneral
-// );
 
 export default routesChanelPosts;
--- routes/chanel/post/create-new-chanel/createNewChanelGeneral.js
+++ routes/chanel/post/create-new-chanel/createNewChanelGeneral.js
@@ -4,13 +4,15 @@ import ChanelModal from "../../../../db/schema/chanel.js";
 const routerCreateNewChanelGeneral = express.Router();
 
 routerCreateNewChanelGeneral.post("/", async (req, res) => {
-  const ReqUserId = req.userId;
-  const { title, name, description } = req.body;
   const userId = req.userId;
+  const { general } = req.body;
+  const { title, name, description } = general;
+  const { tags } = req.body;
+  
   await User.findOne({ _id: userId }).then((docadded) => {
     if (docadded) {
       ChanelModal.create({
-        creator: ReqUserId,
+        creator: userId,
         chanelData: { title, name, description },
       }).then((chanel) => {
         res.json({ "respons-data": chanel });
