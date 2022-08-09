acf2cc333501ddc7b2681bc8d9150d035ef40def
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-08-09 17:04:36 +0200

    DFDF

--- routes/chanel/chanel.js
+++ routes/chanel/chanel.js
@@ -1,9 +1,11 @@
 import express from "express";
+import routesChanelDeletes from "./delete/chanelDeletes.js";
 import routesChanelGet from "./get/chanelgets.js";
 import routesChanelPosts from "./post/chanelposts.js";
 
 const routesChanel = express.Router();
 routesChanel.use("/", routesChanelGet);
 routesChanel.use("/", routesChanelPosts);
+routesChanel.use("/", routesChanelDeletes);
 
 export default routesChanel;
