931b21c6323315b0c5a244577f255eac489dfeeb
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-08-11 19:39:22 +0200

    SDSD

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 app.use(cookieParser());
 
 cors(
-  { "Access-Control-Allow-Origin": `*` },
+  // { "Access-Control-Allow-Origin": `*` },
   "Access-Control-Allow-Methods: POST, PUT, PATCH, GET, DELETE, OPTIONS",
   "Access-Control-Allow-Headers: Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization"
 );
