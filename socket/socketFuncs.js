783781361df35809b155d8b230b14962c1e2e32d
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-08-20 13:15:32 +0200

    DSFD

--- socket/socketFuncs.js
+++ socket/socketFuncs.js
@@ -2,7 +2,6 @@ import streamingVideo from "./streaming/streamingVideo.js";
 const socketFuncs = (io, socket) => {
   io.on("connection", () => {
     streamingVideo(io, socket);
-    console.log("connected");
   });
 };
 
