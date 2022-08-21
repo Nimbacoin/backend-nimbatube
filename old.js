bc9d52d1095c13cbb896315eaec142f03b3acb30
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-08-21 03:37:53 +0200

    SDSDSDSDSDSD

--- old.js
+++ old.js
@@ -8,15 +8,17 @@ import bodyParser from "body-parser";
 import dbConnect from "./db/dbConnect.js";
 import cookieParser from "cookie-parser";
 import AuthToken from "./utils/verify-user/VerifyUser.js";
-import uploadChanelImages from "./routes/chanel/post/uploads/uploadChanelImages.js";
 import multer from "multer";
 import path from "path";
 import { cloudinary } from "./utils/Cloudinary/Cloudinary.js";
+import webrtc from "wrtc";
+import socketFuncs from "./socket/socketFuncs.js";
 
+let senderStream;
 const app = express();
 const PORT = process.env.PORT || 5000;
 const ORIGIN = process.env.ORIGIN;
-
+//
 dotenv.config();
 app.use(cookieParser());
 app.use(express.json());
@@ -33,120 +35,87 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: ORIGIN },
 });
+
 io.on("connection", (socket) => {
-  socket.on("stream", (image) => {
-    socket.broadcast.emit("streaming", image);
-  });
-  socket.on("radio", function (blob) {
-    // can choose to broadcast it to whoever you want
-    socket.broadcast.emit("voice", blob);
-  });
+  //socketFuncs(io, socket);
 });
 
-// app.use(
-//   bodyParser.json({
-//     limit: "50mb",
-//   })
-// );
+app.use(
+  bodyParser.json({
+    limit: "50mb",
+  })
+);
 
-// app.use(
-//   bodyParser.urlencoded({
-//     // limit: "50mb",
-//     // parameterLimit: 100000,
-//     extended: true,
-//   })
-// );
+app.use(
+  bodyParser.urlencoded({
+    // limit: "50mb",
+    // parameterLimit: 100000,
+    extended: true,
+  })
+);
 
-// app.use(function (req, res, next) {
-//   res.setHeader("Access-Control-Allow-Origin", `*`);
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS,  PUT,PATCH, DELETE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type, scrolling, a_custom_header"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   next();
-// });
-const Multer = multer({
-  storage: multer.diskStorage({}),
-  fileFilter: (req, file, cb) => {
-    let ext = path.extname(file.originalname);
-    if (
-      ext !== ".mp4" &&
-      ext !== ".mkv" &&
-      ext !== ".jpeg" &&
-      ext !== ".jpg" &&
-      ext !== ".png"
-    ) {
-      a++;
-      cb(new Error("File type is not supported"), false);
-      return;
-    }
-    cb(null, true);
-    console.log(a);
-  },
-});
-var storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}_${file.originalname}`);
-  },
-  fileFilter: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    if (ext !== ".mp4") {
-      return cb(res.status(400).end("only jpg, png, mp4 is allowed"), false);
-    }
-    cb(null, true);
-  },
-});
-
-var upload = multer({ storage: storage }).single("file");
-const uploadVideo = (req, res) => {
-  console.log(a);
-  console.log(req.file.path);
-  cloudinary.uploader.upload(
-    req.file.path,
-    {
-      resource_type: "video",
-      folder: "video",
-    },
-
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send(err);
-      } else {
-        console.log(result);
-      }
-    }
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", `*`);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS,  PUT,PATCH, DELETE"
   );
-};
-app.post("/api/video/uploadfiles", (req, res) => {
-  console.log(req.body);
-  console.log(req.files);
-  console.log(req.file);
-  console.log(req.body.file);
-
-  const haha = upload(req, res, (err) => {
-    if (err) {
-      return { success: false, err };
-    }
-    return {
-      data: req.file,
-    };
-  });
-  console.log(haha);
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, scrolling, a_custom_header"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
 
-app.get("/api", AuthToken, (req, res) => {
-  res.json("non");
-});
 app.use("/", Routes);
+
+app.post("/consumer", async ({ body }, res) => {
+  const peer = new webrtc.RTCPeerConnection({
+    iceServers: [
+      {
+        urls: "stun:stun.l.google.com:19302",
+      },
+    ],
+  });
+  const desc = new webrtc.RTCSessionDescription(body.sdp);
+  await peer.setRemoteDescription(desc);
+  senderStream
+    .getTracks()
+    .forEach((track) => peer.addTrack(track, senderStream));
+  const answer = await peer.createAnswer();
+  await peer.setLocalDescription(answer);
+  const payload = {
+    sdp: peer.localDescription,
+  };
+  console.log("here view");
+  res.json(payload);
+});
+
+app.post("/broadcast", async ({ body }, res) => {
+  const peer = new webrtc.RTCPeerConnection({
+    iceServers: [
+      {
+        urls: "stun:stun.l.google.com:19302",
+      },
+    ],
+  });
+  peer.ontrack = (e) => handleTrackEvent(e, peer);
+  const desc = new webrtc.RTCSessionDescription(body.sdp);
+  await peer.setRemoteDescription(desc);
+  const answer = await peer.createAnswer();
+  await peer.setLocalDescription(answer);
+  const payload = {
+    sdp: peer.localDescription,
+  };
+  console.log("here make");
+
+  res.json(payload);
+});
+
+function handleTrackEvent(e, peer) {
+  senderStream = e.streams[0];
+}
 
 server.listen(PORT, (err) => {
   if (err) console.log(err);
