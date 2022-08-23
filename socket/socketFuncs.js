bde448d2cd923cca69c4b203cc742be673a24c23
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-08-23 17:46:58 +0200

    SD

--- socket/socketFuncs.js
+++ socket/socketFuncs.js
@@ -1,67 +1,129 @@
 import streamingVideo from "./streaming/streamingVideo.js";
-let users = {};
+let users = [];
 
-let socketToRoom = {};
-
-const maximum = 2;
-
+let rooms = [];
+let broadcaster;
 const socketFuncs = (io, socket) => {
-  socket.on("join_room", (data) => {
-    console.log("yes here");
-    if (users[data.room]) {
-      const length = users[data.room].length;
-      if (length === maximum) {
-        socket.to(socket.id).emit("room_full");
-        return;
-      }
-      users[data.room].push({ id: socket.id });
-    } else {
-      users[data.room] = [{ id: socket.id }];
-    }
-    socketToRoom[socket.id] = data.room;
+  socket.on("broadcaster", (id) => {
+    broadcaster = id;
+    console.log("broadcaster set", broadcaster);
+    socket.emit("broadcaster", broadcaster);
+    socket.broadcast.emit("new-broadcaster", socket.id);
+  });
+  socket.on("watcher", (broadcasterId) => {
+    socket.to(broadcasterId).emit("watcher", socket.id);
+    console.log("watcher set", socket.id);
+    console.log("broadcasterId", broadcasterId);
+  });
+  socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+    console.log("offer sent", message);
+  });
+  socket.on("answer", (id, message) => {
+    socket.to(id).emit("answer", socket.id, message);
+    console.log("answer sent");
+  });
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+    console.log("candidate", message);
+  });
 
+  socket.on("close", () => {
+    console.log("closed socket");
+  });
+
+  // socket.on("new-broadcaster", (broadcaster) => {
+  //   socket.broadcast.emit("active-broadcaster", broadcaster);
+  //   console.log("active-broadcaster emitted");
+  // });
+
+  socket.on("watcher-disconnect", () => {
+    console.log("watcher disconnected");
+    socket.emit("disconnectPeer", socket.id);
+  });
+
+  socket.on("new message", (data) => {
+    console.log(data.room);
+    socket.broadcast.to(data.room).emit("receive message", data);
+  });
+  socket.on("room", (data) => {
+    console.log("room join");
+    console.log(data);
     socket.join(data.room);
-    console.log(`[${socketToRoom[socket.id]}]: ${socket.id} enter`);
-
-    const usersInThisRoom = users[data.room].filter(
-      (user) => user.id !== socket.id
-    );
-
-    console.log(users);
-
-    io.sockets.to(socket.id).emit("all_users", usersInThisRoom);
   });
 
-  socket.on("offer", (sdp) => {
-    console.log("offer: " + socket.id);
-    socket.broadcast.emit("getOffer", sdp);
+  socket.on("leave room", (data) => {
+    console.log("leaving room");
+    console.log(data);
+    socket.leave(data.room);
   });
+  // socket.on("cearte_room", (data) => {
+  //   const roomId = data.room;
+  //   socket.join(data.room);
+  //   const filtered = rooms.filter((rm) => rm.roomId === data.room);
+  //   if (filtered.length) {
+  //     rooms[0].socketId = socket.id;
+  //     rooms[0].room = roomId;
+  //     console.log("hey ", data.room, "socket changed", socket.id);
+  //   } else if (roomId.length) {
+  //     rooms.push({
+  //       room: data.room,
+  //       socketId: socket.id,
+  //     });
+  //     console.log("hey ", data.room, "you just creatd your room");
+  //     console.log(rooms);
+  //   }
+  //   users.push({ userId: data.room, socketId: socket.id });
+  // });
+  // socket.on("join_room", (data) => {
+  //   const roomId = data.room;
 
-  socket.on("answer", (sdp) => {
-    console.log("answer: " + socket.id);
-    socket.broadcast.emit("getAnswer", sdp);
-  });
+  //   if (users.length <= 1 && rooms.length >= 1 && roomId.length) {
+  //     users.push({ userId: data.room, socketId: socket.id });
+  //     socket.join(data.room);
+  //     console.log("hey ", data.room, "you just joined a  room");
+  //   }
 
-  socket.on("candidate", (candidate) => {
-    console.log("candidate: " + socket.id);
-    socket.broadcast.emit("getCandidate", candidate);
-  });
+  //   io.sockets.to(socket.id).emit("all_users", users);
+  // });
 
-  socket.on("disconnect", () => {
-    console.log(`[${socketToRoom[socket.id]}]: ${socket.id} exit`);
-    const roomID = socketToRoom[socket.id];
-    let room = users[roomID];
-    if (room) {
-      room = room.filter((user) => user.id !== socket.id);
-      users[roomID] = room;
-      if (room.length === 0) {
-        delete users[roomID];
-        return;
-      }
-    }
-    socket.broadcast.to(room).emit("user_exit", { id: socket.id });
-    console.log(users);
-  });
+  // socket.on("offer", (data) => {
+  //   console.log("offer: " + data.user);
+  //   const socketId = rooms[0]?.socketId;
+  //   // if (socketId) {
+  //   //   console.log("offered to : streamer" + socketId, "rooms", rooms);
+  //   //   io.to(socketId).emit("getOffer", data.sdp);
+  //   //   io.to(socket.id).emit("getOffer", data.sdp);
+  //   // }
+  //   console.log
+  //   socket.broadcast.emit("getOffer", data.sdp);
+  // });
+
+  // socket.on("answer", (data) => {
+  //   console.log("answer: ", data.user);
+  //   socket.broadcast.emit("getAnswer", data.mySdp);
+  // });
+
+  // socket.on("candidate", (candidate) => {
+  //   console.log("candidate: " + socket.id);
+  //   socket.broadcast.emit("getCandidate", candidate);
+  // });
+
+  // socket.on("disconnect", () => {
+  //   const filtered = rooms.filter((rm) => rm.socketId === socket.id);
+  //   const filteredUsers = users.filter((rm) => rm.socketId === socket.id);
+  //   const userIndex = users.findIndex((user) => user.socketId === socket.id);
+  //   if (filtered.length >= 1) {
+  //     rooms = [];
+  //     console.log("you just delete your room");
+  //     console.log(rooms);
+  //   }
+  //   if (filteredUsers.length >= 1 && userIndex >= 0) {
+  //     users.splice(userIndex, 1);
+  //     console.log("you just desconncted");
+  //     console.log("users", users);
+  //   }
+  // });
 };
 
 export default socketFuncs;
