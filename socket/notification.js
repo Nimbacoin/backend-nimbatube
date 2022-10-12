0c689c70e04c3ecd3db427156a5db975254cd6bd
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-10-12 16:00:25 +0200

    DFDFDF

--- socket/notification.js
+++ socket/notification.js
@@ -20,16 +20,16 @@ const notification = (io, socket) => {
         const userTokken = JSON.parse(User);
         const email = userTokken.email;
         const isInUser = users.some(({ email }) => email === email);
-        console.log(isInUser);
+        // console.log(isInUser);
         if (isInUser) {
           const index = users.findIndex(({ email }) => email === email);
-          console.log(index);
+          //  console.log(index);
           if (index >= 0) {
             users[index].socketId = socket.id;
-            console.log("socket id is changed");
+            //  console.log("socket id is changed");
           }
         } else {
-          console.log("new user added", email);
+          // console.log("new user added", email);
           await userModal.findOne({ email: email }).then(async (docadded) => {
             if (docadded) {
               users.push({
@@ -43,13 +43,34 @@ const notification = (io, socket) => {
 
         // console.log("users", users);
         socket.on("notification", async (data) => {
-          await channelModal
+          channelModal
             .findOne({ _id: data?.channelId })
             .then(async (channel) => {
-              console.log(channel);
+              if (channel) {
+                let allOnlineUsers = [];
+                console.log(channel);
+                //console.log("notification", data);
+                const followers = channel.followers;
+                followers.map((user) => {
+                  if (users.some(({ id }) => id === user.id)) {
+                    console.log("user online", user);
+                    //
+                    const indexUser = users.findIndex(
+                      ({ id }) => id === user.id
+                    );
+                    if (indexUser >= 0) {
+                      const idUserOnLine = users[indexUser]?.socketId;
+                      socket
+                        .to(idUserOnLine)
+                        .emit("nofy-new-video", "idUserOnLine", "message");
+                    }
+                    //
+                  }
+                });
+                // users.map(() => {});
+              }
             });
-          console.log("notification", data);
-          users.map(() => {});
+
           //   socket.to(id).emit("offer", socket.id, message);
         });
       }
