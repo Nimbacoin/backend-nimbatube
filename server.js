88de62c6ca970bd11f0fde5a757b56c8399004b6
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-11-03 11:44:33 +0100

    DF

--- server.js
+++ server.js
@@ -26,6 +26,11 @@ let senderStream;
 const app = express();
 const PORT = process.env.PORT || 5000;
 const ORIGIN = process.env.ORIGIN;
+const ORIGINWWW = process.env.ORIGINWWW;
+const ORIGINHTTP = process.env.ORIGINHTTP;
+const ORIGINHTTPWWW = process.env.ORIGINHTTPWWW;
+const ORIGINHTTPS = process.env.ORIGINHTTPS;
+const ORIGINHTTPSWWW = process.env.ORIGINHTTPSWWW;
 //
 dotenv.config();
 app.use(Session);
@@ -43,7 +48,14 @@ dbConnect();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: ORIGIN },
+  cors: {
+    origin: ORIGINWWW,
+    ORIGINHTTP,
+    ORIGINHTTPS,
+    ORIGIN,
+    ORIGINHTTPSWWW,
+    ORIGINHTTPWWW,
+  },
 });
 io.use(ios(Session));
 io.use(
