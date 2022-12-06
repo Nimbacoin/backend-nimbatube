12d4205f5d421bf8f5970646f630830f5ff57579
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-06 14:06:31 +0100

    DF

--- routes/video/videoRoutes.js
+++ routes/video/videoRoutes.js
@@ -1,13 +1,10 @@
 import express from "express";
-import videoRoutesGets from "./get/videoRoutesGets.js";
-import videoRoutesPosts from "./post/videoRoutesPosts.js";
+// import videoRoutesGets from "./get/videoRoutesGets.js";
+// import videoRoutesPosts from "./post/videoRoutesPosts.js";
 const videoRoutes = express.Router();
-// videoRoutes.get("/get/video/display", (rq, res) => {
-//   console.log("video");
-//   res.json([]);
-// });
-videoRoutes.use("/", videoRoutesGets);
-videoRoutes.use("/", videoRoutesPosts);
+
+// videoRoutes.use("/", videoRoutesGets);
+// videoRoutes.use("/", videoRoutesPosts);
 // videoRoutes.use("/", videoRoutesDeletes);
 
 export default videoRoutes;
