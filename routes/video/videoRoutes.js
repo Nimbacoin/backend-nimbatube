a548f6e83566af55c306337743d1dca08b3df383
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-06 14:07:41 +0100

    DF

--- routes/video/videoRoutes.js
+++ routes/video/videoRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-// import videoRoutesGets from "./get/videoRoutesGets.js";
+import videoRoutesGets from "./get/videoRoutesGets.js";
 // import videoRoutesPosts from "./post/videoRoutesPosts.js";
 const videoRoutes = express.Router();
 
-// videoRoutes.use("/", videoRoutesGets);
+videoRoutes.use("/", videoRoutesGets);
 // videoRoutes.use("/", videoRoutesPosts);
 // videoRoutes.use("/", videoRoutesDeletes);
 
