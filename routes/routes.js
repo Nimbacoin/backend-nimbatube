8aafa7767215997edda12eab6832e494b6e8b839
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-06 13:52:16 +0100

    DFDFDF

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ import express from "express";
 const Routes = express.Router();
 import routesAuth from "./auth/auth.js";
 import routesChannel from "./channel/channel.js";
-// import videoRoutes from "./video/videoRoutes.js";
+import videoRoutes from "./video/videoRoutes.js";
 // import tag from "../db/schema/tag.js";
 // import imagesRoutes from "./images/imagesRoutes.js";
 // import liveStream from "./live-stream/liveStream.js";
@@ -11,9 +11,10 @@ import routesChannel from "./channel/channel.js";
 
 Routes.use("/api", routesAuth);
 Routes.use("/api", routesChannel);
+Routes.use("/api", videoRoutes);
 // Routes.use("/api", liveStream);
 // Routes.use("/api", imagesRoutes);
-// Routes.use("/api", videoRoutes);
+
 // Routes.use("/api", cryptoRoutes);
 
 // Routes.use("/api/get/search/all-search", (req, res) => {
