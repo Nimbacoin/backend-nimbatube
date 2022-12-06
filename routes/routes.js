6a72273557a00feec471e7f6db303088cbf57fe7
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-06 13:59:03 +0100

    DF

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ import routesAuth from "./auth/auth.js";
 import routesChannel from "./channel/channel.js";
 // this one has error import videoRoutes from "./video/videoRoutes.js";
 // import tag from "../db/schema/tag.js";
-// import imagesRoutes from "./images/imagesRoutes.js";
+import imagesRoutes from "./images/imagesRoutes.js";
 // import liveStream from "./live-stream/liveStream.js";
 // import searchRoutes from "./search/searchRoutes.js";
 // import cryptoRoutes from "./crypto/cryptoRoutes.js";
@@ -12,8 +12,8 @@ import routesChannel from "./channel/channel.js";
 Routes.use("/api", routesAuth);
 Routes.use("/api", routesChannel);
 // this one has error Routes.use("/api", videoRoutes);
+Routes.use("/api", imagesRoutes);
 // Routes.use("/api", liveStream);
-// Routes.use("/api", imagesRoutes);
 
 // Routes.use("/api", cryptoRoutes);
 
