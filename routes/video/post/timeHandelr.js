a5a47935b4d552dff7343d33e9d3554da1ee795f
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-07 06:27:53 +0100

    DF

--- routes/video/post/timeHandelr.js
+++ routes/video/post/timeHandelr.js
@@ -3,30 +3,29 @@ import videoModal from "../../../db/schema/video.js";
 import VideoTimeReader from "./timer.js";
 import fs from "fs";
 const timeHandelr = async (Id, path) => {
-  console.log(Id, path);
+  VideoTimeReader(20000)
   if (Id) {
     videoModal.findOne({ _id: Id }).then(async (video) => {
       if (video) {
-        console.log(video);
-        await getVideoDurationInSeconds(path).then(async (duration) => {
-          const timeVideo = VideoTimeReader(duration);
-          console.log(timeVideo);
-          const filter = {
-            _id: video._id,
-          };
-          const updateUser = {
-            duration: timeVideo,
-          };
+      //   await getVideoDurationInSeconds(path).then(async (duration) => {
+      //     const timeVideo = VideoTimeReader(duration);
+      //     console.log(timeVideo);
+      //     const filter = {
+      //       _id: video._id,
+      //     };
+      //     const updateUser = {
+      //       duration: timeVideo,
+      //     };
 
-          try {
-            await videoModal.updateOne(filter, updateUser);
-            fs.unlinkSync(path);
-            //file removed
-          } catch (error) {
-            console.log(error);
-          }
-        });
-      }
+      //     try {
+      //       await videoModal.updateOne(filter, updateUser);
+      //       fs.unlinkSync(path);
+      //       //file removed
+      //     } catch (error) {
+      //       console.log(error);
+      //     }
+      //   });
+      // }
     });
   }
 };
