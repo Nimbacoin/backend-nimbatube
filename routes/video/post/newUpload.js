e15793275b6339099ce3693cfbdcc30d50b55bcd
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-07 06:09:14 +0100

    DF

--- routes/video/post/newUpload.js
+++ routes/video/post/newUpload.js
@@ -1,16 +1,16 @@
 import express from "express";
-import User from "../../../db/schema/user.js";
 const newUpload = express.Router();
-import AuthToken from "../../../utils/verify-user/VerifyUser.js";
-import mongoose from "mongoose";
-import multer from "multer";
-import path from "path";
-import videoModal from "../../../db/schema/video.js";
-import fs from "fs";
-import s3UploadVideo from "./upload/aws3.js";
-import timeHandelr from "./timeHandelr.js";
-import channelModal from "../../../db/schema/channel.js";
-const __dirname = path.resolve();
+// import AuthToken from "../../../utils/verify-user/VerifyUser.js";
+// import User from "../../../db/schema/user.js";
+// import mongoose from "mongoose";
+// import multer from "multer";
+// import path from "path";
+// import videoModal from "../../../db/schema/video.js";
+// import fs from "fs";
+// import s3UploadVideo from "./upload/aws3.js";
+// import timeHandelr from "./timeHandelr.js";
+// import channelModal from "../../../db/schema/channel.js";
+// const __dirname = path.resolve();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -28,59 +28,56 @@ newUpload.post(
   AuthToken,
   upload.single("video"),
   async (req, res) => {
-    const channelId = req.body.channelId;
-    if (mongoose.Types.ObjectId.isValid(channelId)) {
-      channelModal.findOne({ id: channelId }).then(async (channel) => {
-        if (channel.creator === req.userId) {
-          const File = req.file;
-          fs.readFile(File.path, async (err, buffer) => {
-            console.log("buffer", buffer);
-
-            const reslt = await s3UploadVideo(
-              buffer,
-              File.originalname,
-              "videos",
-              process.env.AWS_BUCKET_NAME
-            );
-            console.log(reslt, File.path);
-            // try {
-            //   fs.unlinkSync(path);
-            //   //file removed
-            // } catch (err) {
-            //   console.error(err);
-            // }
-            if (reslt && reslt.Location) {
-              const creatoreId = req.userId;
-              await videoModal
-                .create({
-                  channelId,
-                  location: reslt.Location,
-                  creatore: creatoreId,
-                  filename: File.filename,
-                  fileId: File.id,
-                })
-                .then(async (newFile) => {
-                  const update = channel;
-                  try {
-                    const filter = {
-                      _id: channel._id,
-                    };
-
-                    update.channelData.numbers.uploads =
-                      update.channelData.numbers.uploads + 1;
-                    console.log(update.channelData);
-                    console.log(newFile._id, File);
-                    await timeHandelr(newFile._id, File.path);
-                    await channelModal.updateOne(filter, update);
-                  } catch (error) {}
-
-                  res.json({ file: newFile, uploaded: true });
-                });
-            }
-          });
-        }
-      });
-    }
+    //   const channelId = req.body.channelId;
+    //   if (mongoose.Types.ObjectId.isValid(channelId)) {
+    //     channelModal.findOne({ id: channelId }).then(async (channel) => {
+    //       if (channel.creator === req.userId) {
+    //         const File = req.file;
+    //         fs.readFile(File.path, async (err, buffer) => {
+    //           console.log("buffer", buffer);
+    //           const reslt = await s3UploadVideo(
+    //             buffer,
+    //             File.originalname,
+    //             "videos",
+    //             process.env.AWS_BUCKET_NAME
+    //           );
+    //           console.log(reslt, File.path);
+    //           // try {
+    //           //   fs.unlinkSync(path);
+    //           //   //file removed
+    //           // } catch (err) {
+    //           //   console.error(err);
+    //           // }
+    //           if (reslt && reslt.Location) {
+    //             const creatoreId = req.userId;
+    //             await videoModal
+    //               .create({
+    //                 channelId,
+    //                 location: reslt.Location,
+    //                 creatore: creatoreId,
+    //                 filename: File.filename,
+    //                 fileId: File.id,
+    //               })
+    //               .then(async (newFile) => {
+    //                 const update = channel;
+    //                 try {
+    //                   const filter = {
+    //                     _id: channel._id,
+    //                   };
+    //                   update.channelData.numbers.uploads =
+    //                     update.channelData.numbers.uploads + 1;
+    //                   console.log(update.channelData);
+    //                   console.log(newFile._id, File);
+    //                   await timeHandelr(newFile._id, File.path);
+    //                   await channelModal.updateOne(filter, update);
+    //                 } catch (error) {}
+    //                 res.json({ file: newFile, uploaded: true });
+    //               });
+    //           }
+    //         });
+    //       }
+    //     });
+    //   }
   }
 );
 
