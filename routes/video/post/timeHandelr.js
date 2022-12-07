712dae37e008c33cdf26b17c4ee2f24543714768
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-07 06:46:22 +0100

    DF

--- routes/video/post/timeHandelr.js
+++ routes/video/post/timeHandelr.js
@@ -1,4 +1,4 @@
-// import { getVideoDurationInSeconds } from "get-video-duration";
+import { getVideoDurationInSeconds } from "get-video-duration";
 // import videoModal from "../../../db/schema/video.js";
 // import VideoTimeReader from "./timer.js";
 import videoModal from "../../../db/schema/video.js";
@@ -8,25 +8,25 @@ const timeHandelr = async (Id, path) => {
   if (Id) {
     videoModal.findOne({ _id: Id }).then(async (video) => {
       console.log("sd");
-      //     if (video) {
-      //       await getVideoDurationInSeconds(path).then(async (duration) => {
-      //         const timeVideo = VideoTimeReader(duration);
-      //         console.log(timeVideo);
-      //         const filter = {
-      //           _id: video._id,
-      //         };
-      //         const updateUser = {
-      //           duration: timeVideo,
-      //         };
-      //         try {
-      //           await videoModal.updateOne(filter, updateUser);
-      //           fs.unlinkSync(path);
-      //           //file removed
-      //         } catch (error) {
-      //           console.log(error);
-      //         }
-      //       });
-      //     }
+      if (video) {
+        await getVideoDurationInSeconds(path).then(async (duration) => {
+          //         const timeVideo = VideoTimeReader(duration);
+          //         console.log(timeVideo);
+          //         const filter = {
+          //           _id: video._id,
+          //         };
+          //         const updateUser = {
+          //           duration: timeVideo,
+          //         };
+          //         try {
+          //           await videoModal.updateOne(filter, updateUser);
+          //           fs.unlinkSync(path);
+          //           //file removed
+          //         } catch (error) {
+          //           console.log(error);
+          //         }
+        });
+      }
     });
   }
 };
