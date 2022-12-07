33507375497767941ccf6e0700247b834f96498b
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-12-07 06:31:51 +0100

    DF

--- routes/video/post/timeHandelr.js
+++ routes/video/post/timeHandelr.js
@@ -1,33 +1,31 @@
 // import { getVideoDurationInSeconds } from "get-video-duration";
-import videoModal from "../../../db/schema/video.js";
+// import videoModal from "../../../db/schema/video.js";
 // import VideoTimeReader from "./timer.js";
-import fs from "fs";
+// import fs from "fs";
 const timeHandelr = async (Id, path) => {
-  //VideoTimeReader(20000)
-  if (Id) {
-    videoModal.findOne({ _id: Id }).then(async (video) => {
-      if (video) {
-      //   await getVideoDurationInSeconds(path).then(async (duration) => {
-      //     const timeVideo = VideoTimeReader(duration);
-      //     console.log(timeVideo);
-      //     const filter = {
-      //       _id: video._id,
-      //     };
-      //     const updateUser = {
-      //       duration: timeVideo,
-      //     };
-
-      //     try {
-      //       await videoModal.updateOne(filter, updateUser);
-      //       fs.unlinkSync(path);
-      //       //file removed
-      //     } catch (error) {
-      //       console.log(error);
-      //     }
-      //   });
-      // }
-    });
-  }
+  // if (Id) {
+  //   videoModal.findOne({ _id: Id }).then(async (video) => {
+  //     if (video) {
+  //       await getVideoDurationInSeconds(path).then(async (duration) => {
+  //         const timeVideo = VideoTimeReader(duration);
+  //         console.log(timeVideo);
+  //         const filter = {
+  //           _id: video._id,
+  //         };
+  //         const updateUser = {
+  //           duration: timeVideo,
+  //         };
+  //         try {
+  //           await videoModal.updateOne(filter, updateUser);
+  //           fs.unlinkSync(path);
+  //           //file removed
+  //         } catch (error) {
+  //           console.log(error);
+  //         }
+  //       });
+  //     }
+  //   });
+  // }
 };
 
 export default timeHandelr;
