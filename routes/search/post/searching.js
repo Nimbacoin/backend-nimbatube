0afadb45070f3db78d0b8b06651d72f5679b1abb
mohadeev <94206815+mohadeev@users.noreply.github.com>
2023-01-21 15:01:26 +0100

    DF

--- routes/search/post/searching.js
+++ routes/search/post/searching.js
@@ -4,49 +4,20 @@ import User from "../../../db/schema/user.js";
 const searching = express.Router();
 
 searching.post("/", async (req, res) => {
-  const tr = 1600;
-  const double = 60;
-  const single = 40;
+  const searchData = req.body.searchData;
+  const channels = await channelModal.find({});
 
-  const sdfsdfdf = new Array(20).fill().map((_, i) => {
-    const bb = i + 1;
-    return bb;
-  });
-
-  function multiplesOf(numList, num) {
-    return numList.filter(function (n) {
-      return n % num === 0;
+  if (channels?.length >= 1 && searchData?.length >= 1) {
+    const filtered = channels.filter(({ channelData }) => {
+      console.log(channelData);
+      return (
+        channelData?.name?.includes(searchData) ||
+        channelData?.title?.includes(searchData) ||
+        channelData?.title?.includes(searchData)
+      );
     });
+    console.log("filtered", filtered.length);
   }
-
-  function addondes(numList) {
-    return numList.filter(function (n) {
-      return n % 2 != 0;
-    });
-  }
-
-  const myNewARRAY = multiplesOf(sdfsdfdf, 2);
-  const addondesFunc = addondes(sdfsdfdf);
-
-  myNewARRAY.map((i, ii) => {
-    if (i <= 18) {
-      const newss = (double / 2) * i;
-      const forday = newss * 7;
-      const ddd = forday + 1600;
-      const lastdat = ddd / i;
-    }
-  });
-
-  addondesFunc.map((i, ii) => {
-    if (i <= 18) {
-      const iiiiiii = i - 1;
-      const newss = (double / 2) * iiiiiii;
-      const priceddd = single * 7;
-      const forday = newss * 7;
-      const ddd = forday + priceddd + 1600;
-      const lastdat = ddd / i;
-    }
-  });
 });
 
 export default searching;
